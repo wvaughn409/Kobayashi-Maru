88d7f27e8e54de024900bc31ca75cbc28997985a
wvaughn409 <wvaughn409@yahoo.com>
2020-10-01 23:42:42 -0700

    Update test2.ps1

--- test2.ps1
+++ test2.ps1
@@ -1,1 +1,1 @@
-C:\agent\_work\1\s\7z.exe x -aoa -y "C:\agent\_work\1\s\Kobayashi_Maru\Content.zip" -o"C:\agent\_work\1\s\Kobayashi_Maru\"
+./7z.exe x -aoa -y "C:\agent\_work\1\s\Kobayashi_Maru\Content.zip" -o"C:\agent\_work\1\s\Kobayashi_Maru\"
